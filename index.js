8c5d595302b5e3595cbe6eca4b20c4747bb38be6
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-03 07:05:11 -0400

    commit tree 184cd476c653a8f013043deb04fefed40fc7c687

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let a = 2000
+let b = 100
+
+const divide = (a,b) =>  {
+    return a/b
+};
+
+const square = (param1) => {
+    return param1*param1
+};
+
+const add = (param1, param2) => {
+    return param1 + param2
+};
